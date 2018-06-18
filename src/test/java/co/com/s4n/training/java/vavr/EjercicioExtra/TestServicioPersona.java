6e5ba71740b14b8af4cfcb738d2b19d82f4698ef
julian <julian.t97@hotmail.com>
2018-06-18 00:01:42 -0500

    Ejercicios Extra Test

--- src/test/java/co/com/s4n/training/java/vavr/EjercicioExtra/TestServicioPersona.java
+++ src/test/java/co/com/s4n/training/java/vavr/EjercicioExtra/TestServicioPersona.java
@@ -1,4 +1,27 @@
 package co.com.s4n.training.java.vavr.EjercicioExtra;
 
+import io.vavr.control.Option;
+import org.junit.Test;
+
+
+import static co.com.s4n.training.java.vavr.EjercicioExtra.ServicioPersona.*;
+import static io.vavr.API.Some;
+import static org.junit.Assert.*;
+
+
 public class TestServicioPersona {
+
+    public static final Persona MUJER = new Persona(29,"Juliana","33333",TiposEstadoCivil.Soltero,20000000,Sexo.FEMENINO);
+    public static final Persona HOMBRE = new Persona(35,"Julian","333343",TiposEstadoCivil.Soltero,20000000,Sexo.MASCULINO);
+    public static final Persona OTRO = new Persona(35,"Raul","333343",TiposEstadoCivil.Soltero,200000030,Sexo.OTRO);
+
+
+    @Test
+    public void testAdoptarNiniosParejaHetero(){
+        Option<Persona> resultado = adoptarNinios(MUJER,HOMBRE);
+        Persona ninio = new Persona(0,"Miguel","399393",TiposEstadoCivil.Soltero,0,Sexo.MASCULINO);
+        boolean validar = resultado.get().equals(ninio);
+        assertEquals(resultado.getOrElse(()->null),ninio);
+    }
+
 }
